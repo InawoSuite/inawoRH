f8fc8e62931913ab9b5cfbc3f6cafd47f1904266
royessor06 <savyroyessor@gmail.com>
2026-02-20 08:45:22 +0100

    Sous-menu Avance et Pret

---
+++ src/pages/Pages/Rh/AvanceEtPretAdd.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AvanceEtPretForm from "./AvanceEtPretForm";
+
+const AvanceEtPretAdd = () => {
+    return <AvanceEtPretForm mode="add" />;
+};
+
+export default AvanceEtPretAdd;
